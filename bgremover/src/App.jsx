812b3381e7d7db00a6ecf7eb2a2f9e7fe8ccbe5b
chengivan2 <chengoleivan@gmail.com>
2023-12-28 09:07:25 +0300

    Added drag and drop handler

--- bgremover/src/App.jsx
+++ bgremover/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import FileUpload from './fileUpload'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="main-header">
+        <h2>BG-REMOVE</h2>
+        <button className="main-cta"><a>Login</a></button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="main-text">
+        <h2>Free Background Remover</h2>
+        <p>Free simple background remover. 
+            Try it Today</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+     <div className='drag-drop-container'>
+     <div className='drag-drop-box'>
+        <div className="dotted-border">
+          <FileUpload/ >
+          </div>
+        
+      </div>
+     </div>
     </>
   )
 }
 
-export default App
+export default App;
